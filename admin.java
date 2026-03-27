8c7df983663341a0623144d40ca2611648f67788
cutecode04 <bernaomb@gmail.com>
2026-03-27 00:47:24 -0400

    Ajout des classes Paiement et Admin

---
+++ admin.java
@@ -0,0 +1,55 @@
+public class admin {
+    private String id;
+    private String nom;
+    private String email;
+    private String motDePasse;
+
+    public admin(String id, String nom, String email, String motDePasse) {
+        this.id = id;
+        this.nom = nom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    @Override
+    public String toString() {
+        return "admin{" +
+                "id='" + id + '\'' +
+                ", nom='" + nom + '\'' +
+                ", email='" + email + '\'' +
+                ", motDePasse='" + motDePasse + '\'' +
+                '}';
+    }
+}
